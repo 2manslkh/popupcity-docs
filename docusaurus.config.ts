82db06306be59f95039f101630a845ae61319ded
Kenk <kenghin_lim@hotmail.com>
2024-11-09 11:10:32 +0700

    feat: update github link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/your-org/popup-city-guide/tree/main/',
+            'https://github.com/2manslkh/popupcity-docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -89,7 +89,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/your-org/popup-city-guide',
+              href: 'https://github.com/2manslkh/popupcity-docs',
             },
           ],
         },
